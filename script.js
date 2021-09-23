96472a11d4f42a1fec13288f8e90a8891ad29af3
Leshon Johnson-Hampton <leshonhampton@yahoo.com>
2021-09-23 00:09:14 -0400

    Created and finished script.js file within the LiteralMathMethodExercise folder.

---
+++ script.js
@@ -0,0 +1,26 @@
+// 1a.
+let warmHugs = "Hi, my name is Olaf and I like warm hugs.";
+// 1b.
+console.log(warmHugs.toUpperCase());
+// 1c.
+console.log(warmHugs.replace("like", "love"));
+// 2a.
+ let beenImpaled = "Oh, look at that. I've been impaled.";
+// 2b.
+console.log(beenImpaled.slice(18,36));
+// 3a.
+let dotDotDot = "...";
+// 3b.
+let skullBones = `I don't have a skull ${dotDotDot} or bones.`;
+console.log(skullBones);
+// 4.
+console.log(Math.PI);
+// 5.
+let randomNumber = Math.random();
+console.log(randomNumber);
+randomNumber *= 3;
+console.log(randomNumber);
+randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
+randomNumber++;
+console.log(randomNumber);
